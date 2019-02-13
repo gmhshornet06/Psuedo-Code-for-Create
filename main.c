3fa8f7815d6ffac2e2aade12558214d0c6be8f88
gmhshornet06 <47538676+gmhshornet06@users.noreply.github.com>
2019-02-13 13:42:07 -0500

    Psuedo Code for CREATE
    psuedo code from KIPR software

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <kipr/botball.h>
+
+//Drive from disaster relief zone to utility zone
+//Turn left to electrical line
+//Drive forward to electrical line
+//Raise claw to first electrical line
+//Move claw to connect electrical line
+//Lower arm
+//Move forward toward second electrical line
+//Raise claw to second electrical line
+//Move claw to connect second electrical line
+//Move forward towards gas valve
+//Turn towards gas valve
+//Use claw to pick up gas valve
+//Move gas valve to utility zone
+//Use claw to drop gas valve in utility zone
+//Move towards water reclamation unit
+//Turn towards water reclamation unit
+//Use claw to pick up water reclamation unit
+//Place water reclamation unit ocver water 
+//Move towards grey-black centerline
+//Move along the centerline to collect supplies
+//Turn towards disaster relief zone
+//Move supplies to disaster relief zone
